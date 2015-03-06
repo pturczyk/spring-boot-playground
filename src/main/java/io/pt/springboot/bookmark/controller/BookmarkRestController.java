9c4565a390f443a04512484a81ba687d13b31bff
Pawel Turczyk <pawel.turczyk@tieto.com>
2015-03-06 22:29:14 +0100

    replaced returned entity with location header when creating new bookmarks

--- src/main/java/io/pt/springboot/bookmark/controller/BookmarkRestController.java
+++ src/main/java/io/pt/springboot/bookmark/controller/BookmarkRestController.java
@@ -6,14 +6,17 @@ import io.pt.springboot.bookmark.repo.BookmarkRepository;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/bookmark")
@@ -29,7 +32,12 @@ public class BookmarkRestController {
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<?> addBookmark(@RequestBody Bookmark bookmark) {
 		bookmarkRepository.save(bookmark);
-		return new ResponseEntity<Bookmark>(bookmark, HttpStatus.ACCEPTED);
+		
+		return new ResponseEntity<Bookmark>(
+				null, 
+				buildLocationHeader("/{name}", bookmark.getName()), 
+				HttpStatus.ACCEPTED
+			);
 	}
 
 	@RequestMapping(value = "/{name}", method = RequestMethod.DELETE)
@@ -45,6 +53,14 @@ public class BookmarkRestController {
 	@RequestMapping(method = RequestMethod.GET)
 	public Collection<Bookmark> getBookmarks() {
 		return bookmarkRepository.findAll();
+	}
+
+	private MultiValueMap<String, String> buildLocationHeader(String path, String name) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(
+			ServletUriComponentsBuilder.fromCurrentRequest().path(path).buildAndExpand(name).toUri()
+		);
+		return headers;
 	}
 
 	private Bookmark findBookmark(String name) {
