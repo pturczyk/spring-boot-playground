606fa7acceb866a931ce4f17e5471a985d42f3e4
Pawel Turczyk <pawel.turczyk@tieto.com>
2015-03-20 20:39:59 +0100

    added very basic authentication

---
+++ src/main/java/io/pt/springboot/bookmark/security/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package io.pt.springboot.bookmark.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+}
